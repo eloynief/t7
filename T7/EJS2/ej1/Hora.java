82cd77123e8ec24399eaf2a74fe2a1bfaf82d5c3
enieves <enieves@106-02.info2>
2023-03-16 14:39:32 +0100

    gggg

--- T7/EJS2/ej1/Hora.java
+++ T7/EJS2/ej1/Hora.java
@@ -1,8 +1,59 @@
 package ej1;
 
 public class Hora {
-
-	int hora=0,minuto=0,segundo=0;
+	//elementos 
+	private int hora=0,minuto=0,segundo=0;
+	
+	
+	//constructor con parámetros
+	public Hora(int hora,int minuto,int segundo) {
+		//los parametros
+		this.hora=hora;
+		this.minuto=minuto;
+		this.segundo=segundo;
+		
+	}
+	
+	
+	//get
+	public int gethora() {
+		
+		return hora;
+		
+	}
+	
+	
+	//get
+	public int getminuto() {
+		
+		return minuto;
+	}
+	
+	
+	//get
+	public int getsegundo() {
+		
+		return segundo;
+	}
+	
+	
+	
+	//
+	
+	public void incrementaSegundo() {
+		
+		//incrementa ubn segundo a la hora actual
+		segundo=segundo+1;
+		
+		if(segundo>=60) {
+			minuto++;
+		}
+		if(minuto>=60||segundo>=3600) {
+			
+		}
+	}
+	
+	
 	
 	
 	//✅
--- T7/EJS2/ej1/Mainej1.java
+++ T7/EJS2/ej1/Mainej1.java
@@ -4,7 +4,10 @@ public class Mainej1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		
+		
 	}
 
 }
