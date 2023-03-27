27c6274edaf1464379c9e41ad0f839d3dfedd736
enieves <enieves@106-02.info2>
2023-03-27 14:21:18 +0200

    testttt

---
+++ T7/EJS2/ej4/Disco.java
@@ -0,0 +1,28 @@
+package ej4;
+
+public class Disco {
+	//crear variables
+	String codigo="LIBRE", autor, titulo, genero;
+	
+	int duracion;
+	
+	//constructor con parametros (se puede hacer solo)
+	public Disco(String codigo, String autor, String titulo, String genero, int duracion) {
+		super();
+		this.codigo = codigo;
+		this.autor = autor;
+		this.titulo = titulo;
+		this.genero = genero;
+		this.duracion = duracion;
+	}
+	
+	
+	//si tiene cierto valor, el disco tiene valores
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ T7/EJS2/ej4/Main.java
@@ -0,0 +1,28 @@
+package ej4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int cantidad=0;
+		
+		
+		System.out.println("COLECCIÓN DE DISCOS");
+		
+		System.out.println("===================");
+		System.out.println("1. Listado.");
+		System.out.println("2. Nuevo Disco.");
+		System.out.println("3. Modificar.");
+		System.out.println("4. Borrar.");
+		System.out.println("5. Salir.");
+		
+		while (cantidad<2) {
+		
+			cantidad++;
+		}
+		
+		
+	}
+
+}
